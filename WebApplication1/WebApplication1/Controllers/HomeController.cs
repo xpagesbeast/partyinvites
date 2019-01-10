0e30c5cfaa0f9cd5961e953b621af51e7b35b476
Steve Lohja <slohja@canalbarge.com>
2019-01-09 23:31:41 -0600

    Adding a View (static html)

--- WebApplication1/WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -8,9 +8,25 @@ namespace WebApplication1.Controllers
 {
     public class HomeController : Controller
     {
-        public string Index()
+        public ViewResult Index()
         {
-            return "Hello World Slobodan Lohja Spring 2019";
+            /*
+             * When I first edited the Index action method, it returned a string value. 
+             * This meant that MVC did nothing except pass the string value as is to the browser.
+             * Now that the Index method returns a ViewResult, MVC renders a view and returns 
+             * the HTML it produces. I told MVC which view should be used, so it used the 
+             * naming convention to find it automatically. The convention is that the view 
+             * has the name of the action method and is contained in a folder named after 
+             * the controller: /Views/Home/MyView.cshtml.  
+             * 
+             * I can return other results from action methods besides strings and ViewResult 
+             * objects. For example, if I return a RedirectResult, the browser will be 
+             * redirected to another URL. If I return an HttpUnauthorizedResult, 
+             * I force the user to log in. These objects are collectively known 
+             * as action results. The action result system lets you encapsulate 
+             * and reuse common responses in actions.
+             */
+            return View("MyView");
         }
     }
 }
