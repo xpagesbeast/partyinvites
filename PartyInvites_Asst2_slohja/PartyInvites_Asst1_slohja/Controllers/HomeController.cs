9de1c06e85cf013644e925b7abd9953e71a99960
Steve Lohja <slohja@canalbarge.com>
2019-01-26 00:27:54 -0600

    Completed assignment 2

---
+++ PartyInvites_Asst2_slohja/PartyInvites_Asst1_slohja/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PartyInvites_Asst2_slohja.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Title = "My View Page";
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "good Morning" : "Good Afternoon";
+            return View("MyView");
+        }
+    }
+}
