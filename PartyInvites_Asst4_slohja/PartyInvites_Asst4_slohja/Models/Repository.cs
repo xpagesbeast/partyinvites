3da7bb8264f50b06e7d1f34e343b1f17ac8091e1
Slobodan Lohja <slobodan.lohja@ge.com>
2019-02-05 20:43:09 -0600

    Finished Assignment 4

---
+++ PartyInvites_Asst4_slohja/PartyInvites_Asst4_slohja/Models/Repository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PartyInvites_Asst4_slohja.Models
+{
+    public class Repository
+    {
+        private static List<GuestResponse> responses = new List<GuestResponse>();
+
+        public static IEnumerable<GuestResponse> Responses
+        {
+            get
+            {
+                return responses;
+            }
+        }
+
+        public static void AddResponse(GuestResponse response)
+        {
+            responses.Add(response);
+        }
+    }
+}
